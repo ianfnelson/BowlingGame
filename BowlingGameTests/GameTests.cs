1e1911e1dfb0e54198d2d502fed681df84c4cf7d
Ian Nelson <ian@ianfnelson.com>
2014-04-21 08:21:21 +0100

    first test

---
+++ BowlingGameTests/GameTests.cs
@@ -0,0 +1,15 @@
+﻿using BowlingGame;
+using NUnit.Framework;
+
+namespace BowlingGameTests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void TestGutterGame()
+        {
+            var game = new Game();
+        }
+    }
+}
