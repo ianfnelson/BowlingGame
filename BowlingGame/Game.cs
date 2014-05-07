499b236d9fbada9d223712592d25e108350f5271
Ian Nelson <ian@ianfnelson.com>
2014-05-07 19:13:05 +0100

    done

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -1,6 +1,68 @@
-﻿namespace BowlingGame
+﻿using System.Threading;
+
+namespace BowlingGame
 {
     public class Game
     {
+        private readonly int[] rolls = new int[21];
+        private int currentRoll;
+
+        public int Score
+        {
+            get
+            {
+                var score = 0;
+                var frameIndex = 0;
+                for (var frame = 0; frame < 10; frame++)
+                {
+                    if (IsStrike(frameIndex))
+                    {
+                        score += 10 + StrikeBonus(frameIndex);
+                        frameIndex++;
+                    }
+                    else if (IsSpare(frameIndex))
+                    {
+                        score += 10 + SpareBonus(frameIndex);
+                        frameIndex += 2;
+                    }
+                    else
+                    {
+                        score += SumOfBallsInFrame(frameIndex);
+                        frameIndex += 2;
+                    }
+                }
+                return score;
+            }
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 2];
+        }
+
+        private int SumOfBallsInFrame(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
     }
 }
--- BowlingGameTests/GameTests.cs
+++ BowlingGameTests/GameTests.cs
@@ -1,4 +1,5 @@
 ﻿using BowlingGame;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace BowlingGameTests
@@ -6,10 +7,74 @@ namespace BowlingGameTests
     [TestFixture]
     public class GameTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            game = new Game();
+        }
+
+        private Game game;
+
+        private void RollMany(int n, int pins)
+        {
+            for (var ball = 0; ball < n; ball++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        private void RollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5); // Spare
+        }
+
+        private void RollStrike()
+        {
+            game.Roll(10);
+        }
+
+        [Test]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+            game.Score.Should().Be(20);
+        }
+
         [Test]
         public void TestGutterGame()
         {
-            var game = new Game();
+            RollMany(20, 0);
+            game.Score.Should().Be(0);
+        }
+
+        [Test]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            game.Roll(3);
+
+            RollMany(17, 0);
+
+            game.Score.Should().Be(16);
+        }
+
+        [Test]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            game.Roll(3);
+            game.Roll(4);
+            RollMany(16, 0);
+
+            game.Score.Should().Be(24);
+        }
+
+        [Test]
+        public void TestPerfectGame()
+        {
+            RollMany(12,10);
+            game.Score.Should().Be(300);
         }
     }
 }
